619aff562fa907d664ce9baee853c54518b94137
Bart Massey <bart@cs.pdx.edu>
2012-10-17 13:46:14 -0700

    fixed some trivial stuff; ready to start real code

--- tftpread.c
+++ tftpread.c
@@ -1,6 +1,6 @@
 /* Copyright © 2012 Bart Massey */
 
-/* TFTP send program. */
+/* TFTP client read program. */
 
 #include <assert.h>
 #include <stdio.h>
@@ -13,18 +13,24 @@
 
 int main(int argc, char **argv) {
     assert(argc == 2);
+
+    /* XXX Should try for raw IP address first. */
     struct hostent *h = gethostbyname(argv[1]);
     if (!h) {
         herror("bad destination");
         exit(1);
     }
     assert(h->h_addrtype == AF_INET);
+
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    assert(s != -1);
+
     struct sockaddr_in sin;
     sin.sin_family = AF_INET;
     sin.sin_port = htons(69);
+    /* XXX Should try all the addresses in the list. */
     sin.sin_addr.s_addr = *(uint32_t *)h->h_addr;
-    int s = socket(AF_INET, SOCK_DGRAM, 0);
-    assert(s != -1);
     int r = connect(s, (const struct sockaddr *)&sin, sizeof(sin));
     assert(r != -1);
+    /*HERE*/
 }
