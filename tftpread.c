41693b448ba7e51ca10914dfe90d59a0a2ed6130
Bart Massey <bart@cs.pdx.edu>
2012-10-17 20:33:05 -0700

    now can receive 1-block files; need ack and read loop

--- tftpread.c
+++ tftpread.c
@@ -14,13 +14,32 @@
 
 static char msgbuf[512];
 
-int format_rrq(char *filename) {
+static int format_rrq(char *filename) {
     char *modestr = "octet";
     msgbuf[0] = 0;
     msgbuf[1] = 1;
     strcpy(msgbuf + 2, filename);
     strcpy(msgbuf + 2 + strlen(filename) + 1, modestr);
     return 2 + strlen(filename) + 1 + strlen(modestr) + 1;
+}
+
+static void decode_response(int msglen, int blocknum) {
+    /* XXX should check msglen in case of corrupt packet */
+    unsigned short opcode = (msgbuf[0] << 8) | msgbuf[1];
+    switch (opcode) {
+    case 5 /* error */:
+        fprintf(stderr, "error: %s\n", msgbuf + 4);
+        exit(1);
+    case 3 /* data */: {
+        unsigned short actualblock = (msgbuf[2] << 8) | msgbuf[3];
+        if (actualblock != blocknum)
+            return;
+        int r = fwrite(msgbuf + 4, 1, msglen, stdout);
+        assert(r != -1);
+        return; }
+    }
+    fprintf(stderr, "unknown opcode 0x%04x received, giving up\n", opcode);
+    exit(1);
 }
 
 int main(int argc, char **argv) {
@@ -63,6 +82,8 @@ int main(int argc, char **argv) {
     r = recvfrom(s, (void *)msgbuf, sizeof(msgbuf), 0,
                  (struct sockaddr *)&ssin, &ssaddrlen);
     assert(r >= 0);
+
+    decode_response(r, 1);
 
     sin.sin_family = AF_INET;
     sin.sin_port = ssin.sin_port;
