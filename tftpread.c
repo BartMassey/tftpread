599aab00fcaf3a020173f8b130c6b1a7067b4c0f
Bart Massey <bart@cs.pdx.edu>
2012-10-23 01:01:44 -0700

    finished read loop; fixed many bad bugs

--- tftpread.c
+++ tftpread.c
@@ -18,18 +18,18 @@
 #include <unistd.h>
 #include <string.h>
 
-static char msgbuf[512];
+static unsigned char msgbuf[516];
 
 static int format_rrq(char *filename) {
     char *modestr = "octet";
     msgbuf[0] = 0;
     msgbuf[1] = 1;
-    strcpy(msgbuf + 2, filename);
-    strcpy(msgbuf + 2 + strlen(filename) + 1, modestr);
+    strcpy((char *)msgbuf + 2, filename);
+    strcpy((char *)msgbuf + 2 + strlen(filename) + 1, modestr);
     return 2 + strlen(filename) + 1 + strlen(modestr) + 1;
 }
 
-static void decode_response(int msglen, int blocknum) {
+static int process_response(int msglen, int blocknum) {
     /* XXX should check msglen in case of corrupt packet */
     unsigned short opcode = (msgbuf[0] << 8) | msgbuf[1];
     switch (opcode) {
@@ -37,15 +37,24 @@ static void decode_response(int msglen, int blocknum) {
         fprintf(stderr, "error: %s\n", msgbuf + 4);
         exit(1);
     case 3 /* data */: {
-        unsigned short actualblock = (msgbuf[2] << 8) | msgbuf[3];
-        if (actualblock != blocknum)
-            return;
-        int r = fwrite(msgbuf + 4, 1, msglen, stdout);
-        assert(r != -1);
-        return; }
+        int actualblock = (msgbuf[2] << 8) | msgbuf[3];
+        if (actualblock != blocknum) {
+            fprintf(stderr, "received unexpected block %d\n", actualblock);
+            return -1;
+        }
+        int r = fwrite(msgbuf + 4, 1, msglen - 4, stdout);
+        assert(r == msglen - 4);
+        return msglen - 4; }
     }
     fprintf(stderr, "unknown opcode 0x%04x received, giving up\n", opcode);
     exit(1);
+}
+
+void format_ack(int blocknum) {
+    msgbuf[0] = 0;
+    msgbuf[1] = 4;
+    msgbuf[2] = (blocknum >> 8) & 0xff;
+    msgbuf[3] = blocknum & 0xff;
 }
 
 int main(int argc, char **argv) {
@@ -79,11 +88,26 @@ int main(int argc, char **argv) {
                  (struct sockaddr *)&ssin, &ssaddrlen);
     assert(r >= 0);
 
-    decode_response(r, 1);
-
-    sin.sin_family = AF_INET;
-    sin.sin_port = ssin.sin_port;
-    sin.sin_addr.s_addr = *(uint32_t *)h->h_addr;
+    int blocknum = 1;
+    while (1) {
+        int transferred = process_response(r, blocknum);
+        if (transferred == -1)
+            continue;
+        format_ack(blocknum);
+        r = sendto(s, msgbuf, size, 0,
+                   (const struct sockaddr *)&ssin, (socklen_t)sizeof(ssin));
+        if (transferred < 512) {
+#ifdef DEBUG
+            fprintf(stderr, "processing last %d bytes\n", transferred);
+#endif
+            break;
+        }
+        ssaddrlen = sizeof(ssin);
+        r = recvfrom(s, (void *)msgbuf, sizeof(msgbuf), 0,
+                     (struct sockaddr *)&ssin, &ssaddrlen);
+        assert(r >= 0);
+        blocknum++;
+    }
 
     return 0;
 }
